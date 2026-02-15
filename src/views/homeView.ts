b26f9dfad32c63fd5e0709aed06ed4ca011aca70
Dimitar Ivanov <dimitucha2004@gmail.com>
2026-02-15 19:47:09 +0200

    Import the links to each img

--- src/views/homeView.ts
+++ src/views/homeView.ts
@@ -22,24 +22,39 @@ export function homeView(): string {
       <h2 class="home-page-title">Нека да започваме</h2>
       <div class="mini-img-wrapper">
         <div class="mini-img-card">
+          
+        <a href="/computer_networks" data-link class="mini-img-card">
           <img src="./assets/comp_netw.png" alt="comp_net" class="mini-img" />
           <div class="img-overlay-text">Компютърни мрежи</div>
+          </a>
+
         </div>
+          
+        <a href="/video" data-link class="mini-img-card">
         <div class="mini-img-card">
           <img src="./assets/sec_cam.png" alt="security_cam" class="mini-img" />
           <div class="img-overlay-text">Видеонаблюдение</div>
+        </a>
         </div>
         <div class="mini-img-card">
-          <img
+       
+        <a href="/servers" data-link class="mini-img-card">
+        <img
             src="./assets/server.png"
             alt="computer_server"
             class="mini-img"
           />
           <div class="img-overlay-text">Сървъри</div>
+          </a>
+
         </div>
         <div class="mini-img-card">
-          <img src="./assets/hard_fix.png" alt="laptop" class="mini-img" />
-          <div class="img-overlay-text">Поправка на хардуер</div>
+        
+        <a href="/hardware_repair" data-link class="mini-img-card">  
+        <img src="./assets/hard_fix.png" alt="laptop" class="mini-img" />
+        <div class="img-overlay-text">Поправка на хардуер</div>
+        </a>
+        
         </div>
       </div>
     </section>
