612c612086cde57fb87b842b0b2eaafa0fad0208
Dimitar Ivanov <dimitucha2004@gmail.com>
2026-03-04 15:50:14 +0200

    Improve the router with contact page condition + add guard to the app element value

--- src/router.ts
+++ src/router.ts
@@ -1,24 +1,33 @@
-import {homeView} from "./views/homeView.ts";
-import {computerNetworksView} from "./views/computerNetworksView.ts";
-import {aboutView} from "./views/aboutView.ts";
-import {hardwareRepairView} from "./views/hardwareRepairView.ts";
-import {partnersView} from "./views/partnersView.ts";
-import {serversView} from "./views/serversView.ts";
-import {videoView} from "./views/videoView.ts";
+import { homeView } from "./views/homeView.ts";
+import { computerNetworksView } from "./views/computerNetworksView.ts";
+import { aboutView } from "./views/aboutView.ts";
+import { hardwareRepairView } from "./views/hardwareRepairView.ts";
+import { partnersView } from "./views/partnersView.ts";
+import { serversView } from "./views/serversView.ts";
+import { videoView } from "./views/videoView.ts";
+import contactView from "./views/contactView.ts";
 
-const routes: Record<string, ()=> string> = {
-    "/": homeView, aboutView,
-    "/about": aboutView,
-    "/partners": partnersView,
-    "/computer_networks": computerNetworksView,
-    "/servers": serversView,
-    "/video": videoView,
-    "/hardware_repair": hardwareRepairView
+const routes: Record<string, () => string> = {
+  "/": homeView,
+  "/about": aboutView,
+  "/partners": partnersView,
+  "/computer_networks": computerNetworksView,
+  "/servers": serversView,
+  "/video": videoView,
+  "/hardware_repair": hardwareRepairView,
 };
 
-export function router(){
-    const path = window.location.pathname;
-    const view = routes[path] || homeView;
+export function router() {
+  const path = window.location.pathname;
+  const appEl = document.getElementById("app");
+  if (!appEl) {
+    console.error("Element #app not found in the DOM!");
+    return;
+  }
+  if (path === "/contact") {
+    appEl.innerHTML = `<div class="only-contact">${contactView()}</div>`;
+  }
+  const view = routes[path] || homeView;
 
-    document.getElementById("app")!.innerHTML = view();
+  document.getElementById("app")!.innerHTML = view();
 }
